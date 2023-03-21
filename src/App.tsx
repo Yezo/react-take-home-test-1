51970160d58d6ef35232287cc1d0147ce20864ef
Kev <sulpeum@gmail.com>
2023-03-20 21:06:13 -0400

    edit: title

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,11 @@
-import React from 'react';
+import React from "react"
 
 function App() {
   return (
     <div className="App container">
-      <h1 className='text-center'>Brew Ninja Test App</h1>
+      <h1 className="text-center">Brew Ninja Contact Manager</h1>
     </div>
-  );
+  )
 }
 
-export default App;
+export default App
